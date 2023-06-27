bc4fff470730b1e7daaaf541795d7c798ac86fea
subhajitsikder18 <137827477+subhajitsikder18@users.noreply.github.com>
2023-06-27 23:35:09 +0530

    Create tplink_driver.sh

---
+++ tplink_driver.sh
@@ -0,0 +1,8 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install bc -y
+sudo apt-get install build-essential -y
+sudo apt-get install libelf-dev -y
+sudo apt-get install linux-headers-`uname -r` -y
+sudo apt install dkms -y
+sudo apt install realtek-rtl88xxau-dkms -y 
